506aa5d3304c209bb4b406e2eb52e944932efc06
Andrew Linzie <linzieandrew1@gmail.com>
2026-03-31 18:23:16 -0500

    configure helm provider with eks auth for argocd

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -47,6 +47,23 @@ provider "kubernetes" {
 
 # Helm provider - references kubernetes provider configuration
 provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        var.aws_region
+      ]
+    }
+  }
 }
 
 module "vpc" {
