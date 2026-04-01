908f9cb2392f7a495fefbe1a7c869b04d3310a60
tconuorah <Terrenceonuorah@gmail.com>
2026-04-01 15:21:06 -0500

    app update

--- app/app.js
+++ app/app.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello all from Node.js!!!");
+  res.send("Hello all from Node.js!!");
 });
 
 app.get("/health", (req, res) => {
